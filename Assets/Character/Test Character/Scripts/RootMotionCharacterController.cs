d50fc8fea5789ed25fc68ba171cb5320d488d649
aug551 <40029922+aug551@users.noreply.github.com>
2021-04-27 13:24:46 -0400

    Teaching Commits

--- Assets/Character/Test Character/Scripts/RootMotionCharacterController.cs
+++ Assets/Character/Test Character/Scripts/RootMotionCharacterController.cs
@@ -5,26 +5,44 @@ using UnityEngine;
 public class RootMotionCharacterController : MonoBehaviour
 {
     private Animator anim;
+    private CharacterController controller;
+
+    [SerializeField] private float jumpForce = 8f;
+    [SerializeField] private float gravity = -9.8f;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        controller = GetComponent <CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Horizontal"))
+        if (controller.isGrounded)
         {
-            anim.SetBool("Move", true);
-            this.transform.eulerAngles = 
-                (Input.GetAxis("Horizontal") > 0) ? new Vector3(0, -90, 0) : new Vector3(0, 90, 0);
+            if (Input.GetButton("Horizontal"))
+            {
+                anim.SetBool("Move", true);
+                this.transform.eulerAngles =
+                    (Input.GetAxis("Horizontal") > 0) ? new Vector3(0, -90, 0) : new Vector3(0, 90, 0);
 
+            }
+            else
+            {
+                anim.SetBool("Move", false);
+            }
+
+            if (Input.GetButtonDown("Jump"))
+            {
+            }
         }
-        else
-        {
-            anim.SetBool("Move", false);
-        }
+
+        Debug.Log(controller.isGrounded);
+
     }
+
 }
