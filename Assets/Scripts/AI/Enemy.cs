08e4f1ee037a6b7f0fcd138363edd8c2a1249a45
LegendTaker <Kevin-charron@outlook.com>
2021-04-29 15:17:34 -0400

    Omegapush
    lul

--- Assets/Scripts/AI/Enemy.cs
+++ Assets/Scripts/AI/Enemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemies : NPC
+public class Enemy : NPC
 {
     //=============================================================================
     // Author: Kevin Charron
