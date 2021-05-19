dc84c912e55cccb7d186be8f3e196a3ffc85bcd6
aug551 <40029922+aug551@users.noreply.github.com>
2021-05-18 21:18:40 -0400

    right click detects closest enemy

--- Assets/TeleAttackDetect.cs
+++ Assets/TeleAttackDetect.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class TeleAttackDetect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> enemiesInRange = new List<GameObject>();
+    private GameObject closest = null;
+    private float distance = 0f;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Enemy"))
+        {
+            enemiesInRange.Add(other.gameObject);
+        }
     }
 
-    // Update is called once per frame
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            enemiesInRange.Remove(other.gameObject);
+        }
+    }
+
     void Update()
     {
-        
+        if (Input.GetButton("Attack 2"))
+        {
+            if (enemiesInRange.Count > 0)
+            {
+                foreach (GameObject obj in enemiesInRange)
+                {
+                    if (closest == null)
+                    {
+                        closest = obj;
+                    }
+                    else
+                    {
+                        distance = Vector3.Distance(this.transform.parent.position, obj.transform.position);
+                        if (distance < Vector3.Distance(this.transform.parent.position, closest.transform.position))
+                        {
+                            closest = obj;
+                        }
+                    }
+                }
+            }
+
+            Debug.Log(closest);
+        }        
     }
 }
