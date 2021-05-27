7add5b4f511ef6631b5c1a1e3afee48af8eb5268
ElBenderRodriguez <58280523+ElBenderRodriguez@users.noreply.github.com>
2021-05-27 16:54:46 -0400

    level 2 and 3

---
+++ Assets/EnemyBall.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBall : MonoBehaviour
+{
+    public int speed;
+    private Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
