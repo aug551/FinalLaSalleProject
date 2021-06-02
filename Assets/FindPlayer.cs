13b9b9addc88aaca60ecc0d17c8e0062cb41c22a
aug551 <40029922+aug551@users.noreply.github.com>
2021-06-02 18:01:11 -0400

    added model and more improvements archer

---
+++ Assets/FindPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindPlayer : MonoBehaviour
+{
+    private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<Archer>().Player.transform;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = player.transform.position;
+        this.transform.localPosition = this.transform.localPosition.normalized / 100;
+    }
+
+    public Transform GetLocalPos() { return this.transform; }
+}
