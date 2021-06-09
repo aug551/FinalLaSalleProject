a81646b59586aecb1fc7e039d0394fc2698fa49f
OneDogArmy <doof5678@gmail.com>
2021-06-09 18:12:16 -0400

    Teleport Finished

--- Assets/Scripts/Character/Teleport.cs
+++ Assets/Scripts/Character/Teleport.cs
@@ -5,41 +5,60 @@ using UnityEngine;
 public class Teleport : MonoBehaviour
 {
     public GameObject player;
-    Teleport homeTele;
+    GameObject homeTele;
     Vector3 lastTele = new Vector3();
+    public bool alreadyTele = false;
+    public string compare;
+    public string actual;
 
     void Start()
     {
-        homeTele = GameObject.Find("HomeTeleport").GetComponent<Teleport>();
+        homeTele = GameObject.Find("HomeTeleport").gameObject;
         player = GameObject.Find("Player");
         lastTele = Vector3.zero;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Teleport"))
         {
-           if(this.gameObject == homeTele.gameObject && lastTele == Vector3.zero)
+           if(other.gameObject == homeTele.gameObject && lastTele == Vector3.zero)
            {
                 Debug.Log("cant tele from home");
            }
            else
            {
-                if (this.gameObject == homeTele.gameObject)
+                compare = other.name;
+                if(!alreadyTele)
                 {
-                    player.gameObject.transform.position = lastTele;
-                    Debug.Log("lastTele");
-                }
-                else
-                {
-                    homeTele.lastTele = player.transform.position;
-                    Debug.Log(player.transform.position);
-                    Debug.Log(homeTele.transform.position);
-                    player.gameObject.transform.position = homeTele.transform.position;
-                    Debug.Log(player.transform.position);
-                    Debug.Log(homeTele.transform.position);
+                    if (other.gameObject == homeTele.gameObject)
+                    {
+                        player.gameObject.transform.position = lastTele;
+                        Debug.Log("lastTele");
+                        alreadyTele = true;
+                        actual = other.name;
+                    }
+                    else
+                    {
+                        lastTele = player.transform.position;
+                        Debug.Log(player.transform.position);
+                        Debug.Log(homeTele.transform.position);
+                        player.gameObject.transform.position = homeTele.transform.position;
+                        Debug.Log(player.transform.position);
+                        Debug.Log(homeTele.transform.position);
+                        alreadyTele = true;
+                        actual = other.name;
+                    }
                 }
            }
         }
-    }         
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(actual != compare)
+        {
+            alreadyTele = false;
+        }
+    }
 }
