b73aad06f245bdf21e655566d85d33a89e9d5fb8
ElBenderRodriguez <58280523+ElBenderRodriguez@users.noreply.github.com>
2021-06-09 14:34:50 -0400

    Mechanical Enemies update

--- Assets/EnemyBall.cs
+++ Assets/EnemyBall.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class EnemyBall : MonoBehaviour
 {
-    public int speed;
+    [SerializeField] int speed; //{ get; set; }
+    private Vector3 direction,leftS,rightS;
     private Rigidbody rigidBody;
+    public enum DirectionSelection { left, right };
+    public DirectionSelection directionSelection;
 
     // Start is called before the first frame update
     void Start()
     {
-      
+        if (directionSelection == DirectionSelection.left)
+        {
+            direction = new Vector3(0, 0, 1);
+        }
+        else if (directionSelection == DirectionSelection.right)
+        {
+
+            direction = new Vector3(0, 0, -1);
+
+        }
+        else direction = Vector3.up;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //transform.Translate(new Vector3(1, 0, 0));
+
+        transform.Translate(direction * speed * Time.deltaTime);
         
     }
+
+    //enum directionEnum
+    //{
+    //    left,
+    //    right,
+
+
+    //}
 }
