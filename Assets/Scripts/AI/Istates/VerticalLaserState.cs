22df207f732fbf88da18cdfb59684084c10fd4e7
LegendTaker <Kevin-charron@outlook.com>
2021-07-08 16:22:56 -0400

    Main Menu

--- Assets/Scripts/AI/Istates/VerticalLaserState.cs
+++ Assets/Scripts/AI/Istates/VerticalLaserState.cs
@@ -22,82 +22,82 @@ public class VerticalLaserState : IState
         //17 lasers total
         canTransition = false;
 
-        //lasers = laserRoom.GetLaserTurrets(new int[] { 7, 8 });
-        ////Initlaser(lasers);
-        //ActivateTheLasers(maxWidth1, lasers);
-        //yield return new WaitForSeconds(2f);
-        //ActivateTheLasers(maxWidth2, lasers);
-        //yield return new WaitForSeconds(2f);
-        //DeActivateTheLasers(lasers);
-        ////Finishedlaser(lasers);
-
-        //lasers = laserRoom.GetLaserTurrets(new int[] { 0, 16 });
-        ////Initlaser(lasers);
-        //ActivateTheLasers(maxWidth1, lasers);
-        //yield return new WaitForSeconds(2f);
-        //ActivateTheLasers(maxWidth2, lasers);
-        //yield return new WaitForSeconds(2f);
-        //DeActivateTheLasers(lasers);
-        ////Finishedlaser(lasers);
-
-        //lasers = laserRoom.GetLaserTurrets(new int[] { 6, 12 });
-        ////Initlaser(lasers);
-        //ActivateTheLasers(maxWidth1, lasers);
-        //yield return new WaitForSeconds(2f);
-        //ActivateTheLasers(maxWidth2, lasers);
-        //yield return new WaitForSeconds(2f);
-        //DeActivateTheLasers(lasers);
-        ////Finishedlaser(lasers);
-
-        //lasers = laserRoom.GetLaserTurrets(new int[] { 6, 12 }, new int[] { 0, 2 });
-        ////Initlaser(lasers);
-        //ActivateTheLasers(maxWidth1, lasers);
-        //yield return new WaitForSeconds(2f);
-        //ActivateTheLasers(maxWidth2, lasers);
-        //yield return new WaitForSeconds(2f);
-        //DeActivateTheLasers(lasers);
-        ////Finishedlaser(lasers);
-
-        //lasers = laserRoom.GetLaserTurrets(new int[] { 6, 12 }, new int[] { 1, 3 });
-        ////Initlaser(lasers);
-        //ActivateTheLasers(maxWidth1, lasers);
-        //yield return new WaitForSeconds(2f);
-        //ActivateTheLasers(maxWidth2, lasers);
-        //yield return new WaitForSeconds(2f);
-        //DeActivateTheLasers(lasers);
-        ////Finishedlaser(lasers);
-
-        //lasers = laserRoom.GetLaserTurrets(new int[] { 6, 12 }, new int[] { 0, 2 });
-        ////Initlaser(lasers);
-        //ActivateTheLasers(maxWidth1, lasers);
-        //yield return new WaitForSeconds(2f);
-        //ActivateTheLasers(maxWidth2, lasers);
-        //yield return new WaitForSeconds(2f);
-        //DeActivateTheLasers(lasers);
+        lasers = laserRoom.GetLaserTurrets(new int[] { 7, 8 });
+        //Initlaser(lasers);
+        ActivateTheLasers(maxWidth1, lasers);
+        yield return new WaitForSeconds(2f);
+        ActivateTheLasers(maxWidth2, lasers);
+        yield return new WaitForSeconds(2f);
+        DeActivateTheLasers(lasers);
         //Finishedlaser(lasers);
+
+        lasers = laserRoom.GetLaserTurrets(new int[] { 0, 16 });
+        //Initlaser(lasers);
+        ActivateTheLasers(maxWidth1, lasers);
+        yield return new WaitForSeconds(2f);
+        ActivateTheLasers(maxWidth2, lasers);
+        yield return new WaitForSeconds(2f);
+        DeActivateTheLasers(lasers);
+        //Finishedlaser(lasers);
+
+        lasers = laserRoom.GetLaserTurrets(new int[] { 6, 12 });
+        //Initlaser(lasers);
+        ActivateTheLasers(maxWidth1, lasers);
+        yield return new WaitForSeconds(2f);
+        ActivateTheLasers(maxWidth2, lasers);
+        yield return new WaitForSeconds(2f);
+        DeActivateTheLasers(lasers);
+        //Finishedlaser(lasers);
+
+        lasers = laserRoom.GetLaserTurrets(new int[] { 6, 12 }, new int[] { 0, 2 });
+        //Initlaser(lasers);
+        ActivateTheLasers(maxWidth1, lasers);
+        yield return new WaitForSeconds(2f);
+        ActivateTheLasers(maxWidth2, lasers);
+        yield return new WaitForSeconds(2f);
+        DeActivateTheLasers(lasers);
+        //Finishedlaser(lasers);
+
+        lasers = laserRoom.GetLaserTurrets(new int[] { 6, 12 }, new int[] { 1, 3 });
+        //Initlaser(lasers);
+        ActivateTheLasers(maxWidth1, lasers);
+        yield return new WaitForSeconds(2f);
+        ActivateTheLasers(maxWidth2, lasers);
+        yield return new WaitForSeconds(2f);
+        DeActivateTheLasers(lasers);
+        //Finishedlaser(lasers);
+
+        lasers = laserRoom.GetLaserTurrets(new int[] { 6, 12 }, new int[] { 0, 2 });
+        //Initlaser(lasers);
+        ActivateTheLasers(maxWidth1, lasers);
+        yield return new WaitForSeconds(2f);
+        ActivateTheLasers(maxWidth2, lasers);
+        yield return new WaitForSeconds(2f);
+        DeActivateTheLasers(lasers);
+        Finishedlaser(lasers);
         canTransition = true;
         theBoss.laserRoomFinished = true;
         yield break;
     }
 
-    //public void Finishedlaser(List<StationaryLaser> stationaryLasers)
-    //{
-    //    foreach (StationaryLaser stationaryLaser in stationaryLasers)
-    //    {
-    //        stationaryLaser.Line.enabled = false;
-    //        stationaryLaser.Box.enabled = false;
-    //    }
+    public void Finishedlaser(List<StationaryLaser> stationaryLasers)
+    {
+        foreach (StationaryLaser stationaryLaser in stationaryLasers)
+        {
+            stationaryLaser.Line.enabled = false;
+            stationaryLaser.Box.enabled = false;
+        }
 
-    //}
+    }
 
-    //public void Initlaser(List<StationaryLaser> stationaryLasers)
-    //{
-    //    foreach (StationaryLaser stationaryLaser in stationaryLasers)
-    //    {
-    //        stationaryLaser.Line.enabled = true;
-    //        stationaryLaser.Box.enabled = true;
-    //    }
-    //}
+    public void Initlaser(List<StationaryLaser> stationaryLasers)
+    {
+        foreach (StationaryLaser stationaryLaser in stationaryLasers)
+        {
+            stationaryLaser.Line.enabled = true;
+            stationaryLaser.Box.enabled = true;
+        }
+    }
 
     public void ActivateTheLasers(float maxsize, List<StationaryLaser> stationaryLasers)
     {
