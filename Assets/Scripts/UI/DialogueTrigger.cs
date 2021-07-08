7eb739a8a7e62ccb43962c094c0ae0bb739215ca
ElBenderRodriguez <58280523+ElBenderRodriguez@users.noreply.github.com>
2021-07-08 10:51:15 -0400

    update Dialogue System

---
+++ Assets/Scripts/UI/DialogueTrigger.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public GameObject text;
+
+
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("Convo started");
+            TriggerDialogue();
+        }
+    }
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueController>().StartDialogue(dialogue);
+
+    }
+
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Hit i to interact");
+
+            text.SetActive(true);
+
+
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                Debug.Log("Convo started");
+                TriggerDialogue();
+            }
+
+        }
+        
+    }
+
+
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            text.SetActive(false);
+        }
+    }
+}
+
+
