1886fb216ebb4b10803d17f304d3f468b3dffa72
ElBenderRodriguez <58280523+ElBenderRodriguez@users.noreply.github.com>
2021-07-24 17:36:21 -0400

    update
    Fire Particles update
    enemy update
    Level 2 update

--- Assets/ParticleDamage.cs
+++ Assets/ParticleDamage.cs
@@ -2,36 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Santiago Alvarez Cardenas
 public class ParticleDamage : MonoBehaviour
 {
+    public Animator anim;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //isGettingBurnt = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        anim.SetBool("onFire", false);
     }
 
 
     private void OnParticleCollision(GameObject other)
     {
        
-        Debug.Log("Its getting hot");
+        
 
         if (other.CompareTag("Fire"))
         {
+
+            Debug.Log("Its getting hot");
+
+            anim.SetBool("onFire", true);
             this.GetComponent<CharacterHealth>().TakeDamage(0.2f);
-        }
+            
+
+        } 
 
         if (other.CompareTag("Electricity"))
         {
             this.GetComponent<CharacterHealth>().TakeDamage(1);
         }
-            
-        
+        if (other == null)
+        {
+            anim.SetBool("onFire", false);
+        }
     }
 }
+
